639b6573bc3c3247e880335476f3884e2029691c
unknown <malkhansinghdewda320@gmail.com>
2026-02-12 00:20:54 +0530

    updated js object practise

--- Frontend/JavaScript/Class_6/ObjectPractise.js
+++ Frontend/JavaScript/Class_6/ObjectPractise.js
@@ -1,16 +1,16 @@
-let user = {
-  name : "Malkhan singh",
-  age : 28,
-  greet : function (){
-   console.log("jay shree Ram")
-  },
-  'is Student': true,
-  adress : {
-    city : "Indore",
-    pincode : 12321,
-    state : "MP"
-  }
-}
+// let user = {
+//   name : "Malkhan singh",
+//   age : 28,
+//   greet : function (){
+//    console.log("jay shree Ram")
+//   },
+//   'is Student': true,
+//   adress : {
+//     city : "Indore",
+//     pincode : 12321,
+//     state : "MP"
+//   }
+// }
 
 // let user = new Object();
 // user.name = "Malkhan Singh"
@@ -22,43 +22,43 @@ let user = {
 // }
 
 // defineProperty in js object working of enumerable,writable,configurable
-Object.defineProperty(user , "country",{
-  value : "India",
-  writable : false
-})
+// Object.defineProperty(user , "country",{
+//   value : "India",
+//   writable : false
+// })
 
-Object.defineProperty(user, "age",{
-    writable : true,
-    enumerable : true,
-    configurable : false
-})
+// Object.defineProperty(user, "age",{
+//     writable : true,
+//     enumerable : true,
+//     configurable : false
+// })
 
-user.age = 25
-delete user.age
+// user.age = 25
+// delete user.age
 
 //  to show the defineProperty output by getOwnPropertyDescriptor(objectName, keyNmae);
 
-console.log(user.country)
-console.log(Object.getOwnPropertyDescriptor(user, "country"))
-console.log(Object.getOwnPropertyDescriptor(user, "age"))
+// console.log(user.country)
+// console.log(Object.getOwnPropertyDescriptor(user, "country"))
+// console.log(Object.getOwnPropertyDescriptor(user, "age"))
 
-console.log(user)
+// console.log(user)
 
-console.log(user.name)
-console.log(user['age'])
-// console.log(user['100']())
-// console.log(user['is Student'])
-console.log(user.adress.state)
-console.log(user.adress.pincode)
-console.log(user.adress.city)
+// console.log(user.name)
+// console.log(user['age'])
+// // console.log(user['100']())
+// // console.log(user['is Student'])
+// console.log(user.adress.state)
+// console.log(user.adress.pincode)
+// console.log(user.adress.city)
 
 //  JSON.parse(jsonData) and JSON.stringyfy(object) 
 
-let a = JSON.stringify(user);
-console.log(a)
-console.log(user)
-let A = JSON.parse(a);
-console.log(A)
+// let a = JSON.stringify(user);
+// console.log(a)
+// console.log(user)
+// let A = JSON.parse(a);
+// console.log(A)
  
 // freeze and seal method working .
 
@@ -70,7 +70,7 @@ console.log(A)
 // user.name = "Rahul"
 // user.school = "loty school ujjain"
 
-console.log(user)
+// console.log(user)
 
 
 // All javaScript object method uses are as follow:
@@ -79,22 +79,22 @@ console.log(user)
 
 // ✅ Object.create()
 
-// 👉 Naya object banata hai kisi existing object ko prototype bana ke
+ // 👉 Naya object banata hai kisi existing object ko prototype bana ke
 
-let student = Object.create(user)
-console.log(student.name)
-student.greet()
-console.log(student.adress.city)
+// let student = Object.create(user)
+// console.log(student.name)
+// student.greet()
+// console.log(student.adress.city)
 
-student.name = "bosdike"
-console.log(student.name)
-console.log(user.name)
+// student.name = "bosdike"
+// console.log(student.name)
+// console.log(user.name)
 
-student.adress.city="ujjain" 
-console.log(student.adress.city)
-console.log(user.adress.city)
+// student.adress.city="ujjain" 
+// console.log(student.adress.city)
+// console.log(user.adress.city)
 
-console.log(student);
+// console.log(student);
 
 
 // ✅ Object.assign()
@@ -102,18 +102,85 @@ console.log(student);
 // 👉 Copy ya merge objects (shallow copy)
 
 
+// let user = {
+//   name : "Malkhan singh Dewda",
+//   age : 21,
+//   greet : function(){
+//     console.log("Jay shree Ram")
+//   },
+//   adress : {
+//     city : "indore",
+//     state: "Mp",
+//     pincode: 456321
+    
+//   }
+// }
+
+// console.log(user)
+// object.create() method create a new empty object
+// let malkhan = Object.create(user)
+// console.log(malkhan === user)
+// malkhan.name="banna";
+// malkhan.adress.city="ujjain"
+// console.log(malkhan.adress.city)
+// console.log(malkhan.name)
+// console.log(malkhan)
+
+// object .assign() is use for shallow copy it is use with spread operator or {} empty object
+// let student = Object.assign(user)  //it can assign direct object not create copy
+// console.log(user===student) //true
+
+//shallow copy Es5
+// let emp = Object.assign({},user)
+// console.log(user===emp); //false
+// emp.adress.city="ujjain"
+// emp.name="ramlal"
+// emp.greet()
+// console.log(emp)
+
+// shalow copy with spread operator Es6
+// let star = {...user}  
+// console.log(user===star)  //false
+// star.age=89;
+// star.greet();
+// star.adress.pincode=423222;
+// console.log(star)
+// console.log(user)
+
+
+// shalow copy in Array
+// Array ke case m Array first levele copy hota h baki sare object ko nested level samjhta h is liye original object m bhi chang kr deta h
+//  let arr = [{name: "hariom sagitra"},user]
+ 
+// let array=[...arr]
+// let array = arr.slice()
+//  let array = Array.from(arr)   //teen tariko se shalow copy hota array of object
+ 
+// array[1].name="banna"
+// console.log(array)
+// console.log(arr)
+// console.log(user)
+
+// ==============================================================================
+
 let user = {
   name : "Malkhan singh Dewda",
   age : 21,
-  greet : function(){
-    console.log("Jay shree Ram")
-  },
+  // greet : function(){
+    // console.log("Jay shree Ram")
+  // },
   adress : {
     city : "indore",
     state: "Mp",
     pincode: 456321
     
   }
+}
+
+let start = {
+  salary: 787877,
+  id : 21233,
+  clg : "MITM"
 }
 
 console.log(user)
@@ -127,10 +194,11 @@ console.log(user)
 // console.log(malkhan)
 
 // object .assign() is use for shallow copy it is use with spread operator or {} empty object
-let student = Object.assign(user)  //it can assign direct object not create copy
-console.log(user===student) //true
+// let student = Object.assign(user)  //it can assign direct object not create copy
+// console.log(user===student) //true
 
 //shallow copy Es5
+// let emp = Object.assign({},start,user)
 // let emp = Object.assign({},user)
 // console.log(user===emp); //false
 // emp.adress.city="ujjain"
@@ -139,24 +207,170 @@ console.log(user===student) //true
 // console.log(emp)
 
 // shalow copy with spread operator Es6
-let star = {...user}  
-console.log(user===star)  //false
-star.age=89;
-star.greet();
-star.adress.pincode=423222;
-console.log(star)
-console.log(user)
+// let star = {...user}  
+// console.log(user===star)  //false
+// star.age=89;
+// star.greet();
+// star.adress.pincode=423222;
+// console.log(star)
+// console.log(user)
 
 
 // shalow copy in Array
 // Array ke case m Array first levele copy hota h baki sare object ko nested level samjhta h is liye original object m bhi chang kr deta h
- let arr = [{name: "hariom sagitra"},user]
+// let arr = [{name: "hariom sagitra"},user]
  
 // let array=[...arr]
 // let array = arr.slice()
- let array = Array.from(arr)   //teen tariko se shalow copy hota array of object
+// let array = Array.from(arr)   //teen tariko se shalow copy hota array of object
  
-array[1].name="banna"
-console.log(array)
-console.log(arr)
+// array[1].name="banna"
+// console.log(array)
+// console.log(arr)
+// console.log(user)
+
+// shallow copy with rexursive functions:-
+
+// function shallow(obj) {
+   
+//   let copy = Array.isArray(obj)? [] : {};
+   
+//   for(let key in obj){
+//     copy[key] = obj[key];
+//   }
+   
+//     return copy;
+// }
+
+// let copy = shallow(user);
+// copy.name = "Rampal baba";
+// copy.adress.city = "ujjain";
+// console.log(copy);
+// console.log(user);
+
+
+// =================================================================
+
+// Deep copy of Object...
+
+// 1.structuredClone(objectName);
+// True deep cop
+// ✔ Supports nested objects, arrays, Date, Map, Set
+
+// let copy = structuredClone(user);
+
+// copy.name="Baba Rampal",
+// copy.adress.city= "ujjain",
+// copy.adress.state = "UP"
+// console.log(copy)
+// console.log(user)
+
+// 2. JSON.parse(JSON.stringify(user))
+// ✔ Works for simple data
+// ❌ Loses functions
+// ❌ Loses undefined, Symbol, Date, Map, Set
+
+// let copy = JSON.parse(JSON.stringify(user))
+
+//   copy.name = "bosdike bete";
+//   copy.adress.city="Ujjain";
+//   console.log(copy)
+//   console.log(user)
+  
+//   3.Manual Recursive Function (Interview Favorite)
+
+// function deepcopy (obj) {
+//   if( obj===null || typeof(obj) !== "object"){
+//     return obj
+//   }
+//   let copy = Array.isArray(obj) ? [] : {};
+  
+//   for(let key in obj){
+//     copy[key] = deepcopy(obj[key])
+//   }
+//   return copy;
+// }
+
+// let copy = deepcopy(user);
+// console.log(copy);
+// copy.name="baba khatuShyam";
+// copy.adress.city ="Ujjain";
+// console.log(copy);
+// console.log(user);
+
+
+// some Important methods of Object:-
+
+// object.key(obj)
+// console.log(Object.keys(user))
+
+// // Object.values(obj)
+// console.log(Object.values(user))
+// console.log(Object.values(user.adress));
+
+// // Object.entries(obj):- find entries
+// console.log(Object.entries(user));
+
+// // Object.hasOwn(obj, vaule): to check only keys exist  or not and not check in nested object ortheir prototype
+// console.log(Object.hasOwn(user, 'adress'));
+
+// Object.defineProperty(obj, key{ set a rule}) : to set internal propert like writable : true ; enumerable : false; and configurable : false etc.
+Object.defineProperty(user, "name", {
+  writable : true,
+  enumerable : false,
+  configurable: true,
+})
+
 console.log(user)
+
+// Object.getOwnPropertydiscriptor(user , key name,) : to check the default property of key or object;
+
+// let m = Object.getOwnPropertyDescriptor(user,"age")
+// let n = Object.getOwnPropertyDescriptors(user)
+// console.log(m)
+// console.log(n)
+
+// Object.frozen(obj):Ye check karti hai ki object freeze hua hai ya nahi.
+//Object.freeze(user)
+// let m = Object.isFrozen(user);
+// console.log(m)
+
+// Object.isSealed(obj); : Ye check karti hai ki object seal hua hai ya nahi
+// Object.seal(user)
+// let m = Object.isSealed(user);
+// console.log(m)
+
+// Object.isExtensible : Ye check karta hai ki object me nayi properties add ho sakti hain ya nahi.
+// let n = Object.isExtensible(user)
+// console.log(n)
+
+// Object.is(value1, value2) : copare two value butthey work defferently from 
+// let x = "abc"
+// let b = "cba"
+// let m=Object.is(x,b)
+// console.log(m)
+
+// Object.toString():- ✔ Object ko string me convert karne ke liye
+// ✔ Logging / debugging ke liye
+// ✔ Custom readable output dene ke liye
+// ✔ Implicit type conversion me
+
+// let user = {
+//   name: "Malkhan",
+//   age: 22,
+//   toString() {
+//     return `${this.name} (${this.age})`;
+//   }
+// };
+
+//  console.log("User: " + user);
+// output:
+// User: Malkhan (22)
+
+console.log("User: " + user);
+
+let m = user.toString()
+console.log(m)
+
+
+// =============================End==============================================
--- Frontend/JavaScript/DSA in JS/script.js
+++ Frontend/JavaScript/DSA in JS/script.js
@@ -1,10 +1,26 @@
-let a = "JAy shree Ram";
+// let a = "JAy shree Ram";
 
-for(let i = 0 ; i<10 ; i++){
-   console.log(a)
+// for(let i = 0 ; i<10 ; i++){
+//    console.log(a)
+// }
+
+// write a programme to check the number is psitive,negative and zero?
+
+let number = prompt("Enter a number to check the num is positive ,negative and Zero")
+if(isNaN(number)){
+    console.log("Invalid input")
 }
-
-// write a programe to print 1 to n ;
+else if(number > 0){
+    console.log(`The ${number} is Positive`)
+}else if(number < 0){
+    console.log(`The ${number} is negative`)
+}else {
+    console.log(`The ${number} is Zero`)
+}
+// else{
+//     alert("plese enter valid input")
+// }
+// // write a programe to print 1 to n ;
 
 // let n= 100
 
@@ -37,20 +53,47 @@ for(let i = 0 ; i<10 ; i++){
 
 // write a programe to print odd and even number to n ?
 
-let n = 100;
+// let n = 100;
 
-for(let i=0; i<=n; i++){
-    if(i % 2 === 0){
-        console.log('even number is' , i)
-    }else{
-        console.log('odd number is', i)
-    }
-}
+// for(let i=0; i<=n; i++){
+//     if(i % 2 === 0){
+//         console.log('even number is' , i)
+//     }else{
+//         console.log('odd number is', i)
+//     }
+// }
 
 
 // write a programe to check the leap year ?
 
-let year = 2020;
-if(year % 4 === 0 || year % 100 === 0 || year % 400 === 0){
-    console.log('this is leap year',year)
-}
+// let year = 2020;
+// if(year % 4 === 0 || year % 100 === 0 || year % 400 === 0){
+//     console.log('this is leap year',year)
+// }
+
+// Q wap to print queue of any no and square of any no.
+//   let num = 9;
+//   let square = num ** 2;
+//   let cube = num ** 3;
+//   console.log(`square of ${num} is : ${square}`)
+//   console.log(`square of ${num} is : ${cube}`)
+
+// Q wap to print 1 to n queue of any no and square of 1 to n no.
+
+// let num = 100;
+// for(let i = 1; i<=num; i++){
+//     let squre = i ** 2
+// console.log(`square of ${i} is ${squre}`)
+// }
+// Q Wap to tell how many no is devisible by 7,11,13 from 1 to 100;
+// let num = 100 
+// let count = 0 ;
+//   for(let i = 1; i <= num; i++){
+//     if(i % 7 === 0 || i % 11 === 0 || i % 13 === 0){
+//         count++
+//         console.log(`number is divided by 7,11,13 is ${i}`)
+//     }
+//   }
+//   console.log(`total number is divided by 7,11,13 is ${count}`)
+// Q Wap to tell how many no is devisible by 3 and 5 both from 1 to 100;(15,30,45,60,75,90)
+// Q Wap to tell maximum no between 5 no.
